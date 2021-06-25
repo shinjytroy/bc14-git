978e98784f2c3fb92877ab0ce409fd24c7f85f38
shinjytroy <85876561+shinjytroy@users.noreply.github.com>
2021-06-25 12:58:01 +0700

    Delete baitap1.js

--- baitap1.js
+++
@@ -1,9 +0,0 @@
-//BÀI TẬP 1:
-//Đầu vào
-var luongNgay = 100000;
-var soNgay = 24;
-//Xử lý
-var tienLuong = luongNgay * soNgay
-//Đầu ra
-console.log("Số ngày làm việc: ", soNgay)
-console.log("Số tiền lương nhân viên: ", tienLuong)
